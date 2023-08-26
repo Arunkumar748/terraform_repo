fabe4453dbd106e93c18791f26b1644e26e92bfc
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-26 19:25:12 +0530

    modifiy ami id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create EC2 Instance
 resource "aws_instance" "abcdef-public_instance" {
-    ami           = "ami-051f7e7f6c2f40dc1" 
+    ami           = "ami-051f7e7f6c2f40dc1"
     instance_type = "t2.micro"
     
     tags = {
