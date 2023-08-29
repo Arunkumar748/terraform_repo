ffd6797e55673017d57b3961277bd34cbd07c4ab
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-29 17:43:47 +0530

    Using NUMBER variable

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "ec2_examble" {
     ami           = "ami-051f7e7f6c2f40dc1"
     #count        =  2
-    instance_type = var.instance_type
+    instance_type = var.instance_count
     #security_groups = [aws_security_group.TF_SG.name]
     tags = {
         Name = "Terraform EC2"
--- variable.tf
+++ variable.tf
@@ -1,5 +1,13 @@
-variable "instance_type" {
+#String Type
+/* variable "instance_type" {
    description = "Instance type t2.micro"   #Meaning full describtion
    type        = string                     #Ex: string, number, bool, list, set, map
    default     = "t2.micro"                 #variable default value
+}  */
+
+#Number Type
+variable "instance_count" {
+   description = "EC2 Instance count"   #Meaning full describtion
+   type        =  number                #Ex: string, number, bool, list, set, map
+   default     =  3                     #variable default value
 } 
