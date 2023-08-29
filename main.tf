fa4c47229bcb333eede19408270af11f9718c676
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-29 17:49:54 +0530

    modify COUNT variables

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-# Create EC2 Instance
+/* # Create EC2 Instance
 resource "aws_instance" "ec2_examble" {
     ami           = "ami-051f7e7f6c2f40dc1"
     #count        =  2
-    instance_type = var.instance_count
+    instance_type = var.instance_type
+    #security_groups = [aws_security_group.TF_SG.name]
+    tags = {
+        Name = "Terraform EC2"
+    }
+  } */
+
+
+resource "aws_instance" "ec2_examble" {
+    ami           = "ami-051f7e7f6c2f40dc1"
+    count        =  var.instance_count
+    instance_type =  "t2.micro"
     #security_groups = [aws_security_group.TF_SG.name]
     tags = {
         Name = "Terraform EC2"
