08679c34e44d1ca49ca0af6736a8fd5717500133
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-29 21:58:34 +0530

    Mentioned sensitive message

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
-output "my_EC2_Public_IP"{
+/* output "my_EC2_Public_IP"{
     value = aws_instance.ec2_example.public_ip
+} */
+
+ output "my_EC2_Public_IP"{
+    value = "This is High sensitive"
+    sensitive  = true
 }
