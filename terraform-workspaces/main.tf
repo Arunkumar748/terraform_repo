1c22086752462037251f42e69d12bd531982f576
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-30 11:30:39 +0530

    remover INVALID PARAMETER VALUE

--- terraform-workspaces/main.tf
+++ terraform-workspaces/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_example" {
 
    ami           = "ami-051f7e7f6c2f40dc1"
-   instance_type =  "var.instance_type"
+   instance_type =  var.instance_type
    
    tags = {
            Name = local.instance_name
