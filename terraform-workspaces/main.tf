68c8686f01a0fee9f421e6912be49cf059cdef57
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-30 22:19:23 +0530

    same code ec2

--- terraform-workspaces/main.tf
+++ terraform-workspaces/main.tf
@@ -1,4 +1,4 @@
-/* resource "aws_instance" "ec2_example" {
+resource "aws_instance" "ec2_example" {
 
    ami           = "ami-051f7e7f6c2f40dc1"
    instance_type =  var.instance_type
@@ -11,4 +11,8 @@
 
 locals {
           instance_name  = "${terraform.workspace}-instance"
-} */
+}
+
+
+#dev
+#test
