268cae92b64ebd0e1f37ccb072375f510179bfb7
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-30 23:24:34 +0530

    Import AMI id added

--- terraform-workspaces/main.tf
+++ terraform-workspaces/main.tf
@@ -16,3 +16,14 @@ locals {
 
 #dev enivronment
 #production environment
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-051f7e7f6c2f40dc1"
+   instance_type =  var.instance_type
+   
+   tags = {
+           Name = import-dev-instance
+   }
+
+}
