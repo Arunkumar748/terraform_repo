53eeb4b1f346e97350555c80e390b5ddf1619eef
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-30 23:40:16 +0530

    change instance id

--- terraform-workspaces/main.tf
+++ terraform-workspaces/main.tf
@@ -29,11 +29,11 @@ locals {
 } */
 
 data "aws_instance" "import-dev-instance" {
-  instance_id = "i-0123456789abcdef0"
+  instance_id = "i-0b647eee4da3d60f2"
 }
 
 resource "aws_instance" "ec2_example" {
-  ami           = "ami-051f7e7f6c2f40dc"
+  ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = var.instance_type
   
   tags = {
