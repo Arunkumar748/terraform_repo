36c8414a61412b8fd942c3d56e08c23b83999e91
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-31 09:50:26 +0530

    added ec2 instance

--- terraform-Import-manual/main.tf
+++ terraform-Import-manual/main.tf
@@ -1,9 +1,9 @@
-/* provider "aws" {
+provider "aws" {
     region = "us-east-1"
 }
 
 resource "aws_instance" "import-ec2-manual" {
-    ami   =  "i-007fd1b924c64b503"
+    ami   =  "ami-005b11f8b84489615"
     instance_type  =  "t2.micro"
     
     tags  =  {
@@ -11,15 +11,15 @@ resource "aws_instance" "import-ec2-manual" {
     }
 
 }
- */
 
 
 
-#ami-026ebd4cfe2c043b2
+
+#ami-005b11f8b84489615
 
 ######################## terraform import command to import resource 
   
 #terraform import <RESOURCE_TYPE>.<RESOURCE_NAME> <RESOURCE_ID> 
-#terraform import aws_instance.import-ec2-manual i-007fd1b924c64b503
+#terraform import aws_instance.import-ec2-manual i-0e5731608f6d187c7
 
 #terraform import aws_s3_bucket.my_bucket my-bucket-name 
