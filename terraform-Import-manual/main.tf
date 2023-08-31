08e68b1c4926b32c5065a294f7a95c43177e777d
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-31 11:46:56 +0530

    added name

--- terraform-Import-manual/main.tf
+++ terraform-Import-manual/main.tf
@@ -2,12 +2,12 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "importec2" {
+resource "aws_instance" "importec2-test" {
     ami   =  "ami-026ebd4cfe2c043b2"
     instance_type  =  "t2.micro"
     
     tags  =  {
-              Name  = "importec2"
+              Name  = "importec2-test"
     }
 
 }
