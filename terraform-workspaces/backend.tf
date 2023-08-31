1fadd9fcf7f18b47a70cb9e25121200ed0356a9e
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-31 22:37:43 +0530

    removed key

---
+++ terraform-workspaces/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "statefile-bucket-31-08"
+    key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
+    region         = "us-east-1"  # Change to your desired region
+    encrypt        = true         # Optional: Set to true for encryption
+    #dynamodb_table = "terraform-state-locks"  # Optional: Use DynamoDB for locking
+  }
+}
+
+#terraform_repo/terraform-workspaces/terraform.tfstate.d/dev
