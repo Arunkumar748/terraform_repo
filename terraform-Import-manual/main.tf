8ee8d514ad69c78fb8f9aaec0edcc6eee4c3ab8f
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-08-31 11:22:09 +0530

    remarks -removed resources

--- terraform-Import-manual/main.tf
+++ terraform-Import-manual/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "importec2" {
-    ami   =  "ami-005b11f8b84489615"
-    instance_type  =  "t2.micro"
+    ami   =  ""
+    instance_type  =  ""
     
     tags  =  {
-              Name  = "importec2"
+              Name  = ""
     }
 
 }
@@ -15,11 +15,18 @@ resource "aws_instance" "importec2" {
 
 
 
-#ami-005b11f8b84489615
+
 
 ######################## terraform import command to import resource 
   
 #terraform import <RESOURCE_TYPE>.<RESOURCE_NAME> <RESOURCE_ID> 
 #terraform import aws_instance.import-ec2-manual i-0e5731608f6d187c7
 
+
+#terraform import aws_instance.import-ec2 i-0e5731608f6d187c7
+#terraform import aws_instance.importec2 i-0e5731608f6d187c7
 #terraform import aws_s3_bucket.my_bucket my-bucket-name 
+
+
+#ami-026ebd4cfe2c043b2
+#i-0e3ad1f66994a3859
