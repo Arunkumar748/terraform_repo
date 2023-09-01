13a651a604db1ba45e029adc527cc3f00adf12c1
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-01 11:36:22 +0530

    enter

--- terraform-remote-state/main.tf
+++ terraform-remote-state/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "terraform-state-file" {
+resource "aws_instance" "ec2_example" {
 
    ami           = "ami-051f7e7f6c2f40dc1"
    instance_type =  "t2.micro"
    
    tags = {
-           Name = terraform-state-file
+           Name = "terraform_ec2import_statefile"
    }
 
 }
