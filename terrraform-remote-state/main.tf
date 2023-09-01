7f755471a584736b49b7529935fae651a977910f
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-01 10:56:35 +0530

    terraform-rmote-state

---
+++ terrraform-remote-state/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "terraform-state-file" {
+
+   ami           = "ami-051f7e7f6c2f40dc1"
+   instance_type =  "t2.micro"
+   
+   tags = {
+           Name = Terraform statefile
+   }
+
+}
