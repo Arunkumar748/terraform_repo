ae46ed8c44c82e93b3c7eff00175d9e14a71d17f
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-01 19:12:47 +0530

    added

--- terraform-lifecycle-rules/main.tf
+++ terraform-lifecycle-rules/main.tf
@@ -17,7 +17,7 @@ resource "aws_eip" "myeip" {
 }
 
 resources "aws_eip_association" "eip_assoc" {
-    instance_id  =  "aws_instance" "ec2_example"
+    instance_id  =  "aws_instance.ec2_example"
     allocation_id = "aws_eip.myeip"
 }
 
