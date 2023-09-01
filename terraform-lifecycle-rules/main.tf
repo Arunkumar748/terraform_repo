ece600f573dc6e4807c8fc6d2a54e64bd35f1f51
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-01 19:09:30 +0530

    added terraform lifecycle code

---
+++ terraform-lifecycle-rules/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-051f7e7f6c2f40dc1"
+   instance_type =  "t2.micro"
+   
+   tags = {
+           Name = "terraform_ec2import_statefile"
+   }
+}
+
+resource "aws_eip" "myeip" {
+    vpc = true  
+}
+
+resources "aws_eip_association" "eip_assoc" {
+    instance_id  =  "aws_instance" "ec2_example"
+    allocation_id = "aws_eip.myeip"
+}
+
+resource "aws_security_group" "TF_SG" {
+  name        = "tc-sg"
+  description = "tc-sg"
+  
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["${aws_eip.myeip.public_ip}/32"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+}
