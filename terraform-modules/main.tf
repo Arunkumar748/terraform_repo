1ca4092e8a87436cadb3e14bf0f5be54da6b0d5a
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-04 16:41:12 +0530

    added terraform modules

---
+++ terraform-modules/main.tf
@@ -0,0 +1,13 @@
+#provider information
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "my-demo-webserver-1" {
+  source = "terraform-modules\module-1"
+}
+
+module "my-test-webserver-2" {
+  source = "terraform-modules\module-2"
+}
+
