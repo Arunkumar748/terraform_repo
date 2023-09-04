886ccd21dbd904463b32bf1ad099c983a5c0b305
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-04 19:32:32 +0530

    added

--- terraform-ssh/main.tf
+++ terraform-ssh/main.tf
@@ -33,6 +33,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 resource "aws_key_pair" "deployer" {
   key_name   = "key-for-terraform"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMJyVPqIUiBOtOBBI7hHsZB7uH9BuiygC5dUy5kRI4ujOBXQ1V7Pg/gfsRI41C+UbXkunU+SacuKaBp51vt/W3NxGpDncc4iOkIZt5sQOiJMANWuZ8/WnZduqv9MP4HZmompnDXnzCKJHC36BPyUUGlLJ/NpURzbLDtqqbFjKOys8+UEAtnnBhWT7x3urjvm8D96cSRSco56NOO/n0wH2Or3ddV9aSYOrrqhIhRewAyUY3ktiBMmRTVTvhBR/dqMNPQgQso4ARQikKnyiBQNXeBMtVxZVd/xdn2XcrlDAwMQ4flbBL7wLndPCIQtUcaRJb8vqcO6Hsl1eOQii+r6lb root@ip-172-31-35-85.ec2.internal"
