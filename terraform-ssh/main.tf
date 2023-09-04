a8a34fc95507ad1f3f4a0652b1e98211fafd1af1
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-04 19:21:29 +0530

    push

---
+++ terraform-ssh/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-051f7e7f6c2f40dc1 "  
+    instance_type = "t2.micro" 
+    key_name= "key-for-terraform"
+    vpc_security_group_ids = [aws_security_group.main.id]
+
+ 
+resource "aws_security_group" "main" {
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+   ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 80
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "HTTP"
+     security_groups  = []
+     self             = false
+     to_port          = 80
+  }
+  ]
+   egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "key-for-terraform"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMJyVPqIUiBOtOBBI7hHsZB7uH9BuiygC5dUy5kRI4ujOBXQ1V7Pg/gfsRI41C+UbXkunU+SacuKaBp51vt/W3NxGpDncc4iOkIZt5sQOiJMANWuZ8/WnZduqv9MP4HZmompnDXnzCKJHC36BPyUUGlLJ/NpURzbLDtqqbFjKOys8+UEAtnnBhWT7x3urjvm8D96cSRSco56NOO/n0wH2Or3ddV9aSYOrrqhIhRewAyUY3ktiBMmRTVTvhBR/dqMNPQgQso4ARQikKnyiBQNXeBMtVxZVd/xdn2XcrlDAwMQ4flbBL7wLndPCIQtUcaRJb8vqcO6Hsl1eOQii+r6lb root@ip-172-31-35-85.ec2.internal"
+}
