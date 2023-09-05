3764fb3f6f1df9a7d64c75dfd30eed69d17aa0ce
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-05 18:51:48 +0530

    changed cidr

--- terraform-modules/module-2/main.tf
+++ terraform-modules/module-2/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "tf-modules-sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/24"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -43,7 +43,7 @@ resource "aws_security_group" "tf-modules-sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [""]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
