2c1481562530b3c53ef711908d497ab079135a6b
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-05 19:37:32 +0530

    removed key

--- terraform-modules/module-1/main.tf
+++ terraform-modules/module-1/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2_module_1" {
 
    ami           = var.ami_id
    instance_type =  var.web_instance_type
-   key_name       = "demokey.pub"                #This is optional one 
+   #key_name       = "demokey.pub"                #This is optional one 
    vpc_security_group_ids = [aws_security_group.main-sg.id]
 
    tags = {
@@ -55,7 +55,7 @@ resource "aws_security_group" "main-sg" {
   }
 }
 
-resource "aws_key_pair" "deployer" {
+/* resource "aws_key_pair" "deployer" {
   key_name   = "demokey.pub"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo4PcrzRPYSuHsX7AfWAtHk2wSS9NClUeLr/5EQXcL05DvOn+EXDGpQvVd1uPDwZrmVCHQR7zFuBssC0dEtoWFBFt6Ipdpwfw3njPe7++AkIUlVT8p8ePaEcF/uhJAkYOq5QgezCrlG7K3H/RD9zVcxv1TfE66D0Nu6Z8M+qDUvCU5RxrBwQ3FX0Vp101urBJgggV6v8kgYxm8Vu7zmEXtZIGspVqvQCtxrbqZ+I57H75K75Dr/MLNd+mFIvEEnd4Lzp18p8lJqsEfHmQQThXC3mIoPx6+DV3Dbl5biYCbKDkHzZaEcLOxZfT7UL+mTz46LOW4OiM1U6q90slnKjgB root@ip-172-31-35-85.ec2.internal"
-  } 
+  }  */
