d03fa1c95eec80219d1a2fb8d0b6d27b3028a029
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-05 19:01:07 +0530

    rename

--- terraform-modules/module-2/main.tf
+++ terraform-modules/module-2/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "ec2_module_1" {
       sudo systemctl status apache2
       sudo systemctl start apache2
       sudo chown -R $USER:$USER /var/www/html
-      sudo echo "<html><body><h1>Hello this is module-1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
+      sudo echo "<html><body><h1>Hello this is module-2 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
       EOF
 }
 
