a412bf687559c132680708ae43e6fbcb17832a77
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-06 19:10:27 +0530

    only changed

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -1,11 +1,12 @@
-/* #provider information
+#provider information
 provider "aws" {
     region = "us-east-1"
 }
 
 module "ec2-module-demo" {   
-  source = ".//module"
-} */
+  source       = ".//modules"
+  ami          = "ami-051f7e7f6c2f40dc1"
+}
 
 
 
--- terraform-modules/modules/variables.tf
+++ terraform-modules/modules/variables.tf
@@ -1,10 +1,12 @@
-/* variable "ami_id" {
+ variable "ami_id" {
     default = "ami-051f7e7f6c2f40dc1"
 }
 
-variable "web_instance_type" {
+variable "instance_type" {
     default = "t2.micro"
-} */
+}
+
+
 
 
 
