342c7337fca480ef9dc3a8ccf5e24d50cc3d618b
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-08 13:30:39 +0530

    added

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "demo-module" {   
-  source       = ".//modules"
+  source       = ".//modules-ec2"
   ami          = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
 }
--- terraform-modules/modules-ec2/variables.tf
+++ terraform-modules/modules-ec2/variables.tf
@@ -1,10 +1,8 @@
 variable "ami_id" {
   description = "The ID of the Amazon Machine Image (AMI) to use for the EC2 instance."
 }
-
 variable "instance_type" {
   description = "The instance type for the EC2 instance."
-  default     = "t2.micro"
 }
 
 
