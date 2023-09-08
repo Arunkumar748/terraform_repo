cee0130466536f528a30a08163e36798d173fc33
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-08 13:24:07 +0530

    modify

---
+++ terraform-modules/modules-ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  description = "The ID of the Amazon Machine Image (AMI) to use for the EC2 instance."
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance."
+  default     = "t2.micro"
+}
+
+
+
+
+
+
+
