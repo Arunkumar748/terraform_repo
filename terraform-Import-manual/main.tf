e46ba46bb32a4e43ee59f1b7270ecc198de47d01
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-13 09:04:49 +0530

    terraform import

--- terraform-Import-manual/main.tf
+++ terraform-Import-manual/main.tf
@@ -1,16 +1,16 @@
-/* provider "aws" {
+provider "aws" {
     region = "us-east-1"
-}
+}   
 
-resource "aws_instance" "" {
-    ami   =  ""
-    instance_type  =  ""
+resource "aws_instance" "import-ec2" {
+    ami   =  "ami-04cb4ca688797756f"
+    instance_type  =  "t2.micro"
     
     tags  =  {
-              Name  = ""
+              Name  = "terraform-import-ec2"
     }
 
-} */
+}
 
 
 
