c53f90eb7e106b0d89bae255c6383635f7343436
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:57:39 +0530

    main.tf added

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -1,9 +1,2 @@
-terraform {
-  backend "s3" {
-    bucket         = "statefile-bucket-31-08"
-    key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+
 
--- worspaces2/main.tf
+++ worspaces2/main.tf
@@ -6,9 +6,15 @@
    tags = {
            Name = local.instance_name
    }
-
 }
-
 locals {
           instance_name  = "${terraform.workspace}-instance"
+}
+terraform {
+  backend "s3" {
+    bucket         = "statefile-bucket-31-08"
+    key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
