81609a83dec08fa2a83905a3ecc1bd0fb410038f
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 14:02:23 +0530

    added SSH key

--- terraform-ssh/main.tf
+++ terraform-ssh/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2_example" {
 
     ami = "ami-04cb4ca688797756f"
     instance_type = "t2.micro" 
-    key_name= "key-for-demo-1.pub"
+    key_name= "key-for-demo-2.pub"
     vpc_security_group_ids = [aws_security_group.main.id]
 
 }
@@ -35,6 +35,6 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "key-for-demo-1.pub"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDY9hGqUVkKouOodmjId5quHexyjNWR1DYmtqG/X612BMSW2YZP1Pbca0hX7eYKq9X5SzGzcCBouFAuTH9cRY/+ewWVx7BJUbuU/PJj9sJORj2DiFyCNy0nf1ALA4vWhVOlm8ciXCTBMrG0MepgOZ3UVfWDelhbpWm+ah39sybPS1v4JNL97LONH4jG0Nkx9fARcoBYezB6/JmXuwpYDnAlaJJ5NGZYAwUUxZ0sv4SSUFBfuqKrxCPKxFM5kKanDPrhMhi/Xl5kWbM8I06ZIEQ/71zdhtsu8aDNuqAzYJKcDZYc1eXzUK2FaRVLtPhiZtkf1tI4/OKh76fIs2yRxzXvSs4tmi87KoO+Wg04b3Lotg7NcfGTs2L9e/DauOnSyYiWrmCVjriFT8fkdIREy9UvJNF2R/T9vq90RF+U0Ou3Aizunyyi9wfn6R6QCiR9hUtofhszRFQI7h0IyxLMIoI6vsz4Rf0g59a/wpgtXxLUOr60imbQuYsw9Z3L2MoIMxM= root@ip-172-31-35-85.ec2.internal"
-  }
+  key_name   = "key-for-demo-2.pub"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIbJnh2hPjeNJNMCVngNSWGb1PCDznnQnFsVJMyxJDRFBsbrie6WBVQPxqOiOLI1ndWYAP1RSTKec63HDniJmv9QQnqoA33eswKfQftyByHf5lweE6CPQbF9Mm1Wa0vcOknNchdhzDystZGE+DLIWQVGl0qrGUMna9qLNpcaNq1TRxtPXbEQ07RgetLD/MHI/+4UNAjmqpCf2tp78S1qgcD0q3mSFgH5053ZDTeZhu5lC8pMdXdrwuW0zEJuefbE1XBeVwhNNB+gXHSQNpWPMTFle6HhhQilnAKXL9tr6S/mGA0vz6wJLwfBL1QyEXpUDtvTZwmlFZ2lvhG7dxLvgf+BvgbV6HCBezIeVUhzMJZ3/X3qFcwW7m8d0fVJnTOGDbF+pds1Kmb8WYVenvRtb74cTuyHSVO4jv9NhOgwdeoDOqAEiyPpiqz35AdMvAepNPCShq3MmGf1E4oq81T9MeVPt5moPB2gw3qaf0si0fQkkAXwSdLC75R35sMVOHRHU= root@ip-172-31-35-85.ec2.internal"
+}
