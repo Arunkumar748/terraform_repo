fc243fd26ec5dc60f6535934f643c93c4302dfa8
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:53:47 +0530

    modifiy

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -1,9 +1,9 @@
-# For the "dev" workspace
 terraform {
   backend "s3" {
     bucket         = "statefile-bucket-31-08"
-    key            = "terraform.tfstate.d/terraform.tfstate"
-    region         = "us-east-1"  
-    encrypt        = true         
-    }
+    key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
+
