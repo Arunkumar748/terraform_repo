1e0a12ea7d97f47e802538cfa47eb6db0e250d20
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 19:01:21 +0530

    modifit

--- worspaces2/main.tf
+++ worspaces2/main.tf
@@ -13,7 +13,7 @@ locals {
 terraform {
   backend "s3" {
     bucket         = "statefile-bucket-31-08"
-    key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
+    key            = "terraform.tfstate.d/my-environment/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
