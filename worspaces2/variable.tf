7c020c92597e0eb36b1c0ceddf1a89d4670ae38c
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:49:50 +0530

    removed variable.tf

--- worspaces2/variable.tf
+++ worspaces2/variable.tf
@@ -2,12 +2,3 @@
   description = "Type of EC2 instance"
   type        = string
 }
-# For the "dev" workspace
-variable "workspace" {
-  default = "dev"
-}
-
-# For for the "test" workspace
-variable "workspace" {
-  default = "test"
-}
