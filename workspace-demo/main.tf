49a5b10bcfff37140055969362eb79278422963d
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 20:18:05 +0530

    added s3

--- workspace-demo/main.tf
+++ workspace-demo/main.tf
@@ -1,4 +1,4 @@
- resource "aws_instance" "ec2_example" {
+/*  resource "aws_instance" "ec2_example" {
 
    ami           = "ami-04cb4ca688797756f"
    instance_type =  var.instance_type
@@ -10,4 +10,12 @@
 
 locals {
           instance_name  = "${terraform.workspace}-instance"
+} */
+
+terraform {
+    backend "s3" {
+    bucket = "myworkspace-backup-statefile"
+    key    = "workspaceBackup/terraform.tfstate"
+    region =  "us-east-1"
+}
 }
