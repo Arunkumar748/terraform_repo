105910db0d55206653e7856078a79a018a77e14b
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 19:55:37 +0530

    modifiy

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -1,9 +1,2 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-environment"
-    key            = "terraform.tfstate.d/my-environment/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+
 
