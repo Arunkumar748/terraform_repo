8073dd47878dc5f64535c2caf99415b74d4ea39a
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:51:31 +0530

    removed one backend

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -2,17 +2,8 @@
 terraform {
   backend "s3" {
     bucket         = "statefile-bucket-31-08"
-    key            = "terraform.tfstate.d/dev/terraform.tfstate"
+    key            = "terraform.tfstate.d/terraform.tfstate"
     region         = "us-east-1"  
     encrypt        = true         
     }
-}
-# For the "test" workspace
-terraform {
-  backend "s3" {
-    bucket         = "statefile-bucket-31-08"
-    key            = "terraform.tfstate.d/test/terraform.tfstate"
-    region         = "us-east-1"  
-    encrypt        = true         
-}
 }
