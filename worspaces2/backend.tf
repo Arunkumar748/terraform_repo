e5cfb421dd787f83843bd82288b992b38dfb0e08
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:25:11 +0530

    modifiy

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "statefile-bucket-31-08"
+    bucket         = "statefile-bucket-3"
     key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
     region         = "us-east-1"  # Change to your desired region
     encrypt        = true         # Optional: Set to true for encryption
