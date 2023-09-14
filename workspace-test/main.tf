84d0e902fe9a7f25678571ddddf5c984960551f7
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 20:48:02 +0530

    changed name

--- workspace-test/main.tf
+++ workspace-test/main.tf
@@ -12,13 +12,13 @@ locals {
           instance_name  = "${terraform.workspace}-instance"
 } */
 
-terraform {
+/* terraform {
     backend "s3" {
     bucket = "myworkspace-backup-statefile"
     key    = "workspaceBackup/terraform.tfstate"
     region =  "us-east-1"
 }
-}
+} */
 
 
 
