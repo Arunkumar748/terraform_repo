bb581f889495a146bda369dbea02e6459d2e612c
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 19:53:43 +0530

    modifiy

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -1,4 +1,4 @@
-/* terraform {
+terraform {
   backend "s3" {
     bucket         = "my-environment"
     key            = "terraform.tfstate.d/my-environment/terraform.tfstate"
@@ -6,13 +6,4 @@
     encrypt        = true
   }
 }
- */
- terraform {
-  backend "s3" {
-    bucket         = "my-environment"
-    key            = "terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
-    region         = "us-east-1"  # Change to your desired region
-    encrypt        = true         # Optional: Set to true for encryption
-    #dynamodb_table = "terraform-state-locks"  # Optional: Use DynamoDB for locking
-  }
-}
+
