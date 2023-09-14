bb037ed4bf57917308b103e0b4f4fac4390bc87b
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:35:09 +0530

    modify variable

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "statefile-bucket-3"
-    key            = "terraform.tfstate.d/${terraform.workspace}"
+    key            = "terraform.tfstate.d/${var.workspace}"
     region         = "us-east-1"
     encrypt        = true
   }
--- worspaces2/variable.tf
+++ worspaces2/variable.tf
@@ -2,3 +2,12 @@
   description = "Type of EC2 instance"
   type        = string
 }
+# For the "dev" workspace
+variable "workspace" {
+  default = "dev"
+}
+
+# For for the "test" workspace
+variable "workspace" {
+  default = "test"
+}
