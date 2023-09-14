9fab64e856e931b1a020591ba08a10b078229084
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-14 18:31:48 +0530

    added backend file

--- worspaces2/backend.tf
+++ worspaces2/backend.tf
@@ -2,8 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "statefile-bucket-3"
     key            = "terraform.tfstate.d/${terraform.workspace}"
-    region         = "us-east-1"  # Change to your desired region
-    encrypt        =  true         # Optional: Set to true for encryption
-    #dynamodb_table = "terraform-state-locks"  # Optional: Use DynamoDB for locking
+    region         = "us-east-1"
+    encrypt        = true
   }
 }
