8a2fa4cc91b95413c659db6184b7855754fc3be8
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-15 19:56:08 +0530

    remote.exec added

--- terraform-provisioner/main.tf
+++ terraform-provisioner/main.tf
@@ -1,5 +1,5 @@
 ####################################################
-#File provisioner
+# FILE-PROVISIONER
 ####################################################
 
 /* provider "aws" {
@@ -20,7 +20,7 @@ provisioner "file" {
 }
 connection {
     type        = "ssh"
-    user        = "ec2-user"                                               #The username for your EC2 instance (it may vary)
+    user        = "ec2-user"                                            #The username for your EC2 instance (it may vary)
     private_key = file("abcd_key")                                      #Provide the path to your private key
     host        = aws_instance.terraform-file-provisioner.public_ip
   }
@@ -77,10 +77,10 @@ resource "aws_key_pair" "deployer" {
 
 
 ##########################################
-#Local.exec
+# LOCAL.EXEC-PROVISIONER
 #########################################
 
-provider "aws" {
+/* provider "aws" {
     region = "us-east-1"
 }
 resource "aws_instance" "terraform-file-provisioner" {
@@ -93,6 +93,59 @@ resource "aws_instance" "terraform-file-provisioner" {
 provisioner "local-exec" {
     command = "echo ${self.private_ip} > private_ip.txt"      #echo The server's IP address
   }
+} */
+
+##################################################
+# REMOTE.EXEC-PROVISIONER
+##################################################
+provider "aws" {
+    region = "us-east-1"
 }
+resource "aws_instance" "terraform-file-provisioner" {
+    ami   =  "ami-04cb4ca688797756f"
+    instance_type  =  "t2.micro"
+    key_name  = "abcd_key"
+    vpc_security_group_ids = [aws_security_group.main.id]
+    tags  =  {
+              Name  = "terraform-file-provisioner   "
+    }
 
-
+provisioner "file" {
+    source      = "testfile" 
+    destination = "/tmp/testfile" 
+}
+connection {
+    type        = "ssh"
+    user        = "ec2-user"                                            #The username for your EC2 instance (it may vary)
+    private_key = file("abcd_key")                                      #Provide the path to your private key
+    host        = aws_instance.terraform-file-provisioner.public_ip
+  }
+}
+resource "aws_security_group" "main" {
+  ingress                = [
+   {
+    cidr_blocks      = [ "0.0.0.0/0", ]
+    description      = ""
+    from_port        = 22
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 22
+  }
+  ]
+  egress = [
+    {
+    cidr_blocks      = [ "0.0.0.0/0", ]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+    }
+  ]
+}
