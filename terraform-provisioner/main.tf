b811d76fd78cc502582a3adebe5f5ff1bbe5062f
Arunkumar748 <arunkumaraccess8@gmail.com>
2023-09-15 19:27:04 +0530

    local.exec used

--- terraform-provisioner/main.tf
+++ terraform-provisioner/main.tf
@@ -1,4 +1,8 @@
-provider "aws" {
+####################################################
+#File provisioner
+####################################################
+
+/* provider "aws" {
     region = "us-east-1"
 }
 resource "aws_instance" "terraform-file-provisioner" {
@@ -50,9 +54,9 @@ resource "aws_security_group" "main" {
   ]
 }
 resource "aws_key_pair" "deployer" {
-  key_name   = "abcd_key"
+  key_name   = "abc_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuDYGx8u/gvfVnd6Toe6EMnTF8w83IlqF+ZRIiEKJs1UWRVmH/1v6919vpareDXkNEzarNVXDZGifHQOP2MaG2gKOMy8YJ3+/pV5la0+3c/4prA7oGWwOqWZIbwO0AMRhC8eEY49AmuRdfw6gLS0nlVoCE37uxjlL7k7oMsdQsIwpPIu0lYvr6+GzyjEUkQLDy048vw9xEeIZFCBL8d9Plk715E2uYN4hmflJLapDAmDrlpqXsLM3A8Yi9V/Aa98wv4vB0aHPIn5gSLdvjC1hajzWazGOd2By6uVxo3P2jtMb1/xdcfKxWKR4g/i506p1I2yPLi0rc6VXFtLZAloC7 root@ip-172-31-35-85.ec2.internal"
-}
+} */
 
 
 
@@ -62,6 +66,33 @@ resource "aws_key_pair" "deployer" {
 
 
 
-#Creating key
+#Create single key
 #aws ec2 create-key-pair --key-name filekey --query 'KeyMaterial' --output text > filekey.pem
 #ssh -i filekey.pem ec2-user@public.ip
+
+#create multiple key
+#ssh-keygen -t rsa -b 2048 
+
+
+
+
+##########################################
+#Local.exec
+#########################################
+
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "terraform-file-provisioner" {
+    ami   =  "ami-04cb4ca688797756f"
+    instance_type  =  "t2.micro"
+    tags  =  {
+              Name  = "terraform-file-provisioner   "
+    }
+
+provisioner "local-exec" {
+    command = "echo The server's IP address is ${self.private_ip}"
+  }
+}
+}
+
